795cc8b927541851dc3f821f533606c9799e40ea
JoshG-OTU <joshuageelenfamily@gmail.com>
2025-11-21 16:54:22 -0500

    Aim

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class Aim : MonoBehaviour
 {
+    public float offset = 1f;
+    public float offsetZ = 1f;
+
     void Update()
     {
         Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        transform.localPosition = Vector3.ClampMagnitude(new(Mathf.Sin(Mathf.PI * direction.x / 2), Mathf.Sin(Mathf.PI * direction.y / 2), offsetZ), 1.0f) * offset;
 
     }
 }
